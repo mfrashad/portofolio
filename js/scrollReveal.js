4c926301609b908e2a45834c974bc84e3e0f90d6
mfrashad <m.fathyrashad@gmail.com>
2018-03-16 19:44:57 +0800

    add typing, scrollReveal

---
+++ js/scrollReveal.js
@@ -0,0 +1,5 @@
+window.sr = ScrollReveal();
+sr.reveal('.edu-grid', {duration:1000});
+sr.reveal('#skills-icons > div', {duration:1000},50);
+sr.reveal('.jm-item-wrapper',{duration:1000},100);
+//sr.reveal('#projects', {duration:1000});
